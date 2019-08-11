cea4dbb3bbb4ec06e4dbf09f8d77e0291d4769e1
Hai Nguyen <nduyhai@users.noreply.github.com>
2019-08-11 18:41:22 +0700

    add repos test

--- todo/server/repository/todo_test.go
+++ todo/server/repository/todo_test.go
@@ -3,6 +3,8 @@
 package repository
 
 import (
+	"fmt"
+	uuid "github.com/satori/go.uuid"
 	"testing"
 	"time"
 
@@ -30,18 +32,18 @@ func (s *ToDoRepositorySuite) SetupSuite() {
 	})
 
 	// Create Table
-	s.db.CreateTable(&pb.Todo{}, nil)
+	_ = s.db.CreateTable(&pb.Todo{}, nil)
 
 	s.todoRep = ToDoImpl{DB: s.db}
 }
 
 func (s *ToDoRepositorySuite) TearDownSuite() {
-	s.db.DropTable(&pb.Todo{}, nil)
-	s.db.Close()
+	_ = s.db.DropTable(&pb.Todo{}, nil)
+	_ = s.db.Close()
 }
 
 func (s *ToDoRepositorySuite) TestInsert() {
-	item := &pb.Todo{Id: "new_item", Title: "meeting"}
+	item := &pb.Todo{Id: uuid.NewV4().String(), Title: "meeting"}
 	err := s.todoRep.Insert(item)
 
 	s.Nil(err)
@@ -49,6 +51,69 @@ func (s *ToDoRepositorySuite) TestInsert() {
 	newTodo, err := s.todoRep.Get(item.Id)
 	s.Nil(err)
 	s.Equal(item, newTodo)
+}
+
+func (s *ToDoRepositorySuite) TestList() {
+	item := &pb.Todo{Id: uuid.NewV4().String(), Title: "meeting"}
+	err := s.todoRep.Insert(item)
+	s.Nil(err)
+
+	cases := []struct {
+		Name      string
+		Limit     int32
+		Completed bool
+		FnResult  func([]*pb.Todo, error)
+	}{
+		{
+			"With completed true",
+			10,
+			true,
+			func(toDos []*pb.Todo, err error) {
+				fmt.Println(toDos, err)
+				s.Nil(err)
+				s.Nil(toDos)
+			},
+		},
+		{
+			"Struct with completed false",
+			10,
+			false,
+			func(toDos []*pb.Todo, err error) {
+				fmt.Println(toDos, err)
+				s.Nil(err)
+				s.NotNil(toDos)
+			},
+		},
+	}
+
+	for _, test := range cases {
+		s.Suite.Run(test.Name, func() {
+			toDos, err := s.todoRep.List(test.Limit, test.Completed)
+			test.FnResult(toDos, err)
+		})
+	}
+}
+
+func (s *ToDoRepositorySuite) TestGet() {
+	item := &pb.Todo{Id: uuid.NewV4().String(), Title: "meeting"}
+	err := s.todoRep.Insert(item)
+
+	s.Nil(err)
+
+	newTodo, err := s.todoRep.Get(item.Id)
+	s.Nil(err)
+	s.Equal(item, newTodo)
+}
+
+func (s *ToDoRepositorySuite) TestDelete() {
+	item := &pb.Todo{Id: uuid.NewV4().String(), Title: "meeting"}
+	err := s.todoRep.Insert(item)
+
+	s.Nil(err)
+
+	err = s.todoRep.Delete(item.Id)
+
+	s.Nil(err)
 }
 
 func TestToDoRepository(t *testing.T) {
